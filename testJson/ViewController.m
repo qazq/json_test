727283dffb165160cace8de16fed8bc3ad47eda8
qazq.jackie <qazq.jackie@gmail.com>
2014-11-09 19:13:15 +0800

    a example

--- testJson/ViewController.m
+++ testJson/ViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ViewController.h"
+#import "SBJson.h"
 
 @interface ViewController ()
 
@@ -17,6 +18,27 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    NSURL *url = [NSURL URLWithString:@"https://public-api.wordpress.com/rest/v1.1/sites/mrchu.tw/posts/?category=news"];
+    
+    NSMutableURLRequest *request = [[NSMutableURLRequest alloc] init];
+    [request setURL:url];
+    
+    NSError *error;
+    NSURLResponse *response;
+    NSData *urlData = [NSURLConnection sendSynchronousRequest:request returningResponse:&response error:&error];
+    
+    NSString *data = [[NSString alloc]initWithData:urlData encoding:NSUTF8StringEncoding];
+    //NSLog(@"data = %@", data);
+    
+    SBJsonParser *parser = [[SBJsonParser alloc] init];
+    NSDictionary *root = [parser objectWithString:data];
+    NSArray *posts = root[@"posts"];
+    for (NSDictionary *post in posts) {
+        //NSLog(@"post = %@", post);
+        NSString *title = post[@"title"];
+        NSLog(@"title = %@", title);
+    }
 }
 
 - (void)didReceiveMemoryWarning {
